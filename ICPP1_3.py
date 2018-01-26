ec7c9d733ea149a477a5d60b5c53b7cea17d5051
dongzeyuan <dongfujing88@gmail.com>
2018-01-26 20:31:37 +0800

    1-26在家

---
+++ ICPP1_3.py
@@ -0,0 +1,21 @@
+# coding=UTF-8
+
+import math
+
+x1 = float(input('x1 = '))
+y1 = float(input('y1 = '))
+x2 = float(input('x2 = '))
+y2 = float(input('y2 = '))
+
+k = (y1 - y2) / (x1 - x2)
+
+b = (x1 * y1 - x2 * y1) / (x1 - x2)
+
+print('y = {0}x + {1}'.format(k, b))
+
+x3 = float(input('x3 = '))
+y3 = float(input('y3 = '))
+
+distance = abs(k * x3 - y3 + b) / math.sqrt(k**2 + 1)
+
+print('The distance is {0}'.format(distance))
---
+++ ICPP1_4.py
@@ -0,0 +1,7 @@
+# coding=UTF-8
+
+a = int(input('Please enter a number: '))
+
+b = chr(a)
+
+print('The char is ', b)
