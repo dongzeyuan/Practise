ffacc01ee1686e57928e97636fc9f9a253efe446
dongzeyuan <dongfujing88@gmail.com>
2018-02-16 19:13:49 +0800

    2-16

---
+++ CP7/ICCP7_2.py
@@ -0,0 +1,20 @@
+# coding=UTF-8
+# 对例7-13进行试验，调用函数FileCopy(tar_File,res_File)时，给形参传入一个新文件名，分析代码运行的流程
+
+
+def FileCopy(tar_File, res_File):
+    try:
+        f = open(tar_File, 'rb')
+        f2 = open(tar_File, 'wb')
+    except:
+        print('打开文件异常')
+        return -1
+    s = f.read()
+    f2.write(s)
+    f.close()
+    f2.close()
+    return 0
+
+
+if __name__ == '__main__':
+    FileCopy(123.txt, 321.txt)
