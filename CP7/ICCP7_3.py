66137834f3fbee9c7fdd6008afd56bc917e5134a
dongzeyuan <dongfujing88@gmail.com>
2018-02-20 22:05:02 +0800

    有点眉目了

--- CP7/ICCP7_3.py
+++ CP7/ICCP7_3.py
@@ -1,13 +1,13 @@
 # coding=UTF-8
 # 先建立一个文本文件，然后变成把文件中的大写字母替换为小写字母
 
-f = open('123.txt', 'w+')
-f.write('HLLJSDFOOS')
+f = open('123.txt', 'r+')
 while True:
     lines = f.readline()
     print(lines)
     low_lines = lines.lower()
-    f.write(low_lines)
+# '\t'+变量名这种方式要记住
+    f.write('\n'+low_lines)
     if lines == '':
         break
 f.close()
