828f8787577c7d0449967057b0fdaf5f28517747
dongzeyuan <dongfujing88@gmail.com>
2018-03-03 21:38:25 +0800

    fds

---
+++ ui_learning.py
@@ -0,0 +1,16 @@
+# coding=UTF-8
+# test
+
+import wx
+
+class Frame1(wx.Frame):
+    def __init__(self, superior):
+        wx.Frame.__init__(self, parent=superior, title='my window', pos=(100,200),\
+        size=(400,200))
+
+if __name__ == '__main__':
+    app = wx.App()
+    frame = Frame1(none)
+    frame.Show(True)
+
+app.Mainloop()
