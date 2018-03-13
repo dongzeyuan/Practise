9c68bb2d07d2a2eb770c09c6bc09ce93a4bd4e01
dongzeyuan <dongfujing88@gmail.com>
2018-03-13 16:29:11 +0800

    面板

--- Telbook.py
+++ Telbook.py
@@ -26,9 +26,15 @@ class MyFrame(wx.Frame):
     def __init__(self):
         wx.Frame.__init__(self, None, -1, 'Ditap', size=(800,600))
         panel = wx.Panel(self, -1)
-        label = wx.StaticText(panel, -1, '姓名',(20,20))
-        text = wx.TextCtrl(panel, -1, '', (20,30))
-        text.SetInsertionPoint(0)
+        labe_name = wx.StaticText(panel, -1, '姓名',(20,20))
+        text_name = wx.TextCtrl(panel, -1, '', (20,50))
+        text_name.SetInsertionPoint(0)
+        labe_sex = wx.StaticText(panel, -1, '性别',(300,20))
+        text_sex = wx.TextCtrl(panel, -1, '', (300,50))
+        text_sex.SetInsertionPoint(0)
+        labe_tel = wx.StaticText(panel, -1, '电话',(580,20))
+        text_tel = wx.TextCtrl(panel, -1, '', (580,50))
+        text_tel.SetInsertionPoint(0)
 
 if __name__ == '__main__':
     app = wx.App()
