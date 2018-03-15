bda235c6af81f3008893874f4207ee1230575b83
dongzeyuan <dongfujing88@gmail.com>
2018-03-15 20:19:07 +0800

    11

--- CP12/12-1.py
+++ CP12/12-1.py
@@ -18,24 +18,24 @@ class MyFrame(wx.Frame):
 
 
         label_index = wx.StaticText(panel, -1, '编号', pos=(x0, y0))
-        self.num = label_index(panel, -1, "", pos=(x0, y0 + dy), size=(w, 20))
-        self.num.SetEditable(Fales)
+        self.num = wx.TextCtrl(panel, -1, "", pos=(x0, y0 + dy), size=(w, 20))
+        self.num.SetEditable(False)
 
 
         label_name = wx.StaticText(panel, -1, '姓名', pos=(x0 + w + dx, y0))
-        self.name = label_name(panel, -1, "", pos=(x0 + w + dx, y0 + dy), size=(w, 20))
+        self.name = wx.TextCtrl(panel, -1, "", pos=(x0 + w + dx, y0 + dy), size=(w, 20))
 
         label_sex = wx.StaticText(panel, -1, '性别', pos=(x0 + 2*(w + dx), y0))
-        self.sex = label_sex(panel, -1, "", pos=(x0 + 2*(w + dx), y0 + dy), size=(w, 20))
+        self.sex = wx.TextCtrl(panel, -1, "", pos=(x0 + 2*(w + dx), y0 + dy), size=(w, 20))
 
         label_tel = wx.StaticText(panel, -1, '电话', pos=(x0 + 3*(w + dx), y0))
-        self.tel = label_tel(panel, -1, "", pos=(x0 + 3*(w + dx), y0 + dy), size=(w, 20))
+        self.tel = wx.TextCtrl(panel, -1, "", pos=(x0 + 3*(w + dx), y0 + dy), size=(w, 20))
 
         label_qq = wx.StaticText(panel, -1, 'QQ', pos=(x0 + 4*(w + dx), y0))
-        self.qq = label_qq(panel, -1, "", pos=(x0 + 4*(w + dx), y0 + dy), size=(w, 20))
+        self.qq = wx.TextCtrl(panel, -1, "", pos=(x0 + 4*(w + dx), y0 + dy), size=(w, 20))
 
-        label_adres = wx.StaticText(panel, -1, '地址', pos=(x0 + 5(w + dx), y0))
-        self.adres = label_adres(panel, -1, "", pos=(x0 + 5*(w + dx), y0 + dy), size=(w, 20))
+        label_adres = wx.StaticText(panel, -1, '地址', pos=(x0 + 5*(w + dx), y0))
+        self.adres = wx.TextCtrl(panel, -1, "", pos=(x0 + 5*(w + dx), y0 + dy), size=(w, 20))
 
 
 if __name__ == '__main__':
