b556a0c788bf916b9dec26237aeafd602ea8bea6
dongzeyuan <dongfujing88@gmail.com>
2018-03-17 11:10:00 +0800

    3-17

--- CP12/12-1.py
+++ CP12/12-1.py
@@ -3,9 +3,9 @@ import wx
 import wx.grid
 
 
-class MyFrame(wx.Frame):   
+class MyFrame(wx.Frame):
     def __init__(self):
-        
+
         self.m = 0
         x0 = 22
         y0 = 20
@@ -16,82 +16,80 @@ class MyFrame(wx.Frame):
         wx.Frame.__init__(self, None, -1, '通讯录管理小软件', size=(700, 400))
         panel = wx.Panel(self, -1)
 
-
         label_index = wx.StaticText(panel, -1, '编号', pos=(x0, y0))
         self.num = wx.TextCtrl(panel, -1, "", pos=(x0, y0 + dy), size=(w, 20))
         self.num.SetEditable(False)
-<<<<<<< HEAD
-=======
-
->>>>>>> bda235c6af81f3008893874f4207ee1230575b83
 
         label_name = wx.StaticText(panel, -1, '姓名', pos=(x0 + w + dx, y0))
-        self.name = wx.TextCtrl(panel, -1, "", pos=(x0 + w + dx, y0 + dy), size=(w, 20))
+        self.name = wx.TextCtrl(
+            panel, -1, "", pos=(x0 + w + dx, y0 + dy), size=(w, 20))
 
-        label_sex = wx.StaticText(panel, -1, '性别', pos=(x0 + 2*(w + dx), y0))
-        self.sex = wx.TextCtrl(panel, -1, "", pos=(x0 + 2*(w + dx), y0 + dy), size=(w, 20))
+        label_sex = wx.StaticText(panel, -1, '性别', pos=(x0 + 2 * (w + dx), y0))
+        self.sex = wx.TextCtrl(
+            panel, -1, "", pos=(x0 + 2 * (w + dx), y0 + dy), size=(w, 20))
 
-        label_tel = wx.StaticText(panel, -1, '电话', pos=(x0 + 3*(w + dx), y0))
-        self.tel = wx.TextCtrl(panel, -1, "", pos=(x0 + 3*(w + dx), y0 + dy), size=(w, 20))
+        label_tel = wx.StaticText(panel, -1, '电话', pos=(x0 + 3 * (w + dx), y0))
+        self.tel = wx.TextCtrl(
+            panel, -1, "", pos=(x0 + 3 * (w + dx), y0 + dy), size=(w, 20))
 
-        label_qq = wx.StaticText(panel, -1, 'QQ', pos=(x0 + 4*(w + dx), y0))
-        self.qq = wx.TextCtrl(panel, -1, "", pos=(x0 + 4*(w + dx), y0 + dy), size=(w, 20))
-<<<<<<< HEAD
+        label_qq = wx.StaticText(panel, -1, 'QQ', pos=(x0 + 4 * (w + dx), y0))
+        self.qq = wx.TextCtrl(
+            panel, -1, "", pos=(x0 + 4 * (w + dx), y0 + dy), size=(w, 20))
 
-        label_adres = wx.StaticText(panel, -1, '地址', pos=(x0 + 5*(w + dx), y0))
-        self.adres = wx.TextCtrl(panel, -1, "", pos=(x0 + 5*(w + dx), y0 + dy), size=(w, 20))
+        label_adres = wx.StaticText(
+            panel, -1, '地址', pos=(x0 + 5 * (w + dx), y0))
+        self.adres = wx.TextCtrl(
+            panel, -1, "", pos=(x0 + 5 * (w + dx), y0 + dy), size=(w, 20))
 
-        self.insert = wx.Button(panel, label="新增", pos=(6*x0, y0+3*dy), size=(w, 25))
+        self.insert = wx.Button(panel, label="新增", pos=(
+            6 * x0, y0 + 3 * dy), size=(w, 25))
         self.insert.SetToolTipString("新增记录方法:\n1.在文本框中输入数据；\n2.单击此按钮以增加记录")
-        
-        self.delete = wx.Button(panel, label='删除', pos=(6*x0+w+dx, y0+3*dy), size=(w, 25))
+
+        self.delete = wx.Button(panel, label='删除', pos=(
+            6 * x0 + w + dx, y0 + 3 * dy), size=(w, 25))
         self.delete.SetToolTipString("删除记录方法:\n1.选择一行；\n2.单击此按钮以删除。")
 
-        self.update = wx.Button(panel, label='修改', pos=(6*x0+2*(w+dx), y0+3*dy), size=(w, 25))
-        self.update.SetToolTipString("修改记录方法:\n1.选择一行；\n2.在文本框中进行修改；\n3.单击此按钮以保存修改。")
+        self.update = wx.Button(panel, label='修改', pos=(
+            6 * x0 + 2 * (w + dx), y0 + 3 * dy), size=(w, 25))
+        self.update.SetToolTipString(
+            "修改记录方法:\n1.选择一行；\n2.在文本框中进行修改；\n3.单击此按钮以保存修改。")
 
-        self.select = wx.Button(panel, label='查询', pos=(6*x0+3*(w+dx), y0+3*dy), size=(w, 25))
+        self.select = wx.Button(panel, label='查询', pos=(
+            6 * x0 + 3 * (w + dx), y0 + 3 * dy), size=(w, 25))
         self.select.SetToolTipString("查询所有记录")
-
 
         self.Bind(wx.EVT_BUTTON, self.OnInsert, self.insert)
         self.Bind(wx.EVT_BUTTON, self.OnDelete, self.delete)
         self.Bind(wx.EVT_BUTTON, self.OnUpdate, self.update)
         self.Bind(wx.EVT_BUTTON, self.OnSelect, self.select)
 
-
-        self.grid=wx.grid.Grid(panel, pos=(10,140), size=(550,200))
+        self.grid = wx.grid.Grid(panel, pos=(10, 140), size=(550, 200))
         self.grid.Bind(wx.grid.EVT_GRID_RANGE_SELECT, self.OnGridSelect)
 
+        self.conn = sqlite3.connect("D:/Code/Practise/addressBook.db")
+        self.cur = self.conn.cursor()
 
-        self.conn=sqlite3.connect("D:/Code/Practise/addressBook.db")
-        self.cur=self.conn.cursor()
+        self.res = []
 
-        self.res=[]
-
-
-        def OnGridSelect(self,event):
+        def OnGridSelect(self, event):
+            li = self.grid.GetSelectedRows()
+            if len(li) >= 1:
+                print(li[0])
+                tu = self.res[li[0]]
+                self.num.SetValue('%s' % tu[0])
             pass
 
-        def OnInsert(self,event):
+        def OnInsert(self, event):
             pass
 
-        def OnDelete(self,event):
+        def OnDelete(self, event):
             pass
 
-        def OnUpdate(self,event):
-            pass
-        
-        def OnSelect(self,event):
+        def OnUpdate(self, event):
             pass
 
-
-
-=======
-
-        label_adres = wx.StaticText(panel, -1, '地址', pos=(x0 + 5*(w + dx), y0))
-        self.adres = wx.TextCtrl(panel, -1, "", pos=(x0 + 5*(w + dx), y0 + dy), size=(w, 20))
->>>>>>> bda235c6af81f3008893874f4207ee1230575b83
+        def OnSelect(self, event):
+            pass
 
 
 if __name__ == '__main__':
