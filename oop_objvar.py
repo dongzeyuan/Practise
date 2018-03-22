5b084c2c6982b9829df6fa43a9d498325491472c
dongzeyuan <dongfujing88@gmail.com>
2018-03-22 10:35:50 +0800

    3-22

--- oop_objvar.py
+++ oop_objvar.py
@@ -36,3 +36,17 @@ class Robot:
 
 
 droid1 = Robot('R2-D2')
+droid1.say_hi()
+Robot.how_many()
+
+droid2 = Robot("C-3PO")
+droid2.say_hi
+Robot.how_many()
+
+print("\nRobots can do some work here. \n")
+
+print("Robots have finished their work. So let's destroy them.")
+droid1.die()
+droid2.die()
+
+Robot.how_many()
