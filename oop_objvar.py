8b7be9448ca408a0e889d4ae1b1fd679913a699a
dongzeyuan <dongfujing88@gmail.com>
2018-03-22 10:56:20 +0800

    回文

--- oop_objvar.py
+++ oop_objvar.py
@@ -50,3 +50,15 @@ droid1.die()
 droid2.die()
 
 Robot.how_many()
+
+def reverse(text):
+    return text[::-1]
+
+def is_palindrome(text):
+    return text == reverse(text)
+
+something = input("Enter text: ")
+if is_palindrome(something):
+    print("Yes, it is a palindrome.")
+else:
+    print("No,it is not a palindrome.")
