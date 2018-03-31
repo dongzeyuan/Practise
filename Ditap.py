e924c9b82f8b2ab42fdc3197db9d1452cf19af5d
dongzeyuan <dongfujing88@gmail.com>
2018-03-31 20:39:58 +0800

    历史记录？

--- Ditap.py
+++ Ditap.py
@@ -3,10 +3,7 @@
 # license
 
 
-
-
-
-import wx
+'''import wx
 class MyFrame(wx.Frame):
     def __init__(self):
         wx.Frame.__init__(self, None, -1, 'Ditap', size=(1200,900))
@@ -21,5 +18,39 @@ class MyFrame(wx.Frame):
 
 if __name__ == '__main__':
     app = wx.App()
+    MyFrame().Show(True)
+    app.MainLoop()'''
+
+import wx
+
+
+class MyFrame(wx.Frame):
+    def __init__(self):
+        wx.Frame.__init__(self, None, -1, 'Text Entry Example',
+                          size=(300, 250))
+        panel = wx.Panel(self, -1)
+        multiLabel = wx.StaticText(panel, -1, 'Multi-line')
+        multiText = wx.TextCtrl(panel, -1,
+                                'Here is a loooooooooooog line of text set in the control.\n\n'
+                                'See that it wrapped, and that this line is after a blank',
+                                size=(200, 100), style=wx.TE_MULTILINE)
+        multiText.SetInsertionPoint(0)
+
+        richLabl = wx.StaticText(panel, -1, 'Rich Text')
+        richText = wx.TextCtrl(panel, -1,
+                               'If supported by the native control, this is reversed, and this is \
+                               a different font.', size=(200, 100), style=wx.TE_MULTILINE)
+        richText.SetInsertionPoint(0)
+        richText.SetStyle(44, 52, wx.TextAttr('white', 'black'))
+        points = richText.GetFont().GetPointSize()
+        f = wx.Font(points + 3, wx.ROMAN, wx.ITALIC, wx.BOLD, True)
+        richText.SetStyle(68, 82, wx.TextAttr('blue', wx.NullColour, f))
+        sizer = wx.FlexGridSizer(cols=2, hgap=6, vgap=6)
+        sizer.AddMany([multiLabel, multiText, richLabl, richText])
+        panel.SetSizer(sizer)
+
+
+if __name__ == '__main__':
+    app = wx.PySimpleApp()
     MyFrame().Show(True)
     app.MainLoop()
