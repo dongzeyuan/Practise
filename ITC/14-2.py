66a26e8896dbc7eb052176d0def4e0fe45142357
dongzeyuan <dongfujing88@gmail.com>
2018-08-15 15:37:45 +0800

    绘图

--- ITC/14-2.py
+++ ITC/14-2.py
@@ -1,4 +1,5 @@
 import random
+import pylab
 
 
 class Location(object):
@@ -79,10 +80,10 @@ class EWDrunk(Drunk):
 
 
 class styleIterator(object):
-    def __init__(self,styles):
+    def __init__(self, styles):
         self.index = 0
         self.styles = styles
-    
+
     def nextStyle(self):
         result = self.styles[self.index]
         if self.index == len(self.styles) - 1:
@@ -91,9 +92,37 @@ class styleIterator(object):
             self.index += 1
         return result
 
+
 def simAll(drunkKinds, walkLengths, numTrials):
     for dClass in drunkKinds:
         drunkTest(walkLengths, numTrials, dClass)
+
+
+def simDrunk(numTrials, dClass, walkLengths):
+    meanDistances = []
+    for numSteps in walkLengths:
+        print('Starting simulation of ', numSteps, 'steps')
+        trials = simWalks(numSteps, numTrials, dClass)
+        mean = sum(trials) / len(trials)
+        meanDistances.append(mean)
+    return meanDistances
+
+
+def simAll1(drunkinds, walkLengths, numTrials):
+    styleChoice = styleIterator(('m-', 'r:', 'k-'))
+    for dClass in drunkinds:
+        curStyle = styleChoice.nextStyle()
+        print('Starting simulation of ', dClass.__name__)
+        means = simDrunk(numTrials, dClass, walkLengths)
+        pylab.plot(walkLengths, means, curStyle,
+                   label=dClass.__name__)
+        pylab.title('Mean Distance from Origin(' + str(numTrials) + 'trials)')
+        pylab.xlabel('Number of Steps')
+        pylab.ylabel('Distance from origin')
+        pylab.legend(loc='best')
+        pylab.semilogx()
+        pylab.semilogy()
+    pylab.show()
 
 
 def walk(f, d, numSteps):
@@ -133,4 +162,6 @@ def drunkTest(walkLengths, numTrials, dClass):
 
 if __name__ == '__main__':
     drunkTest((10, 100, 1000, 10000), 100, UsualDrunk)
-    simAll((UsualDrunk,ColdDrunk,EWDrunk),(100,1000),10)
+    simAll((UsualDrunk, ColdDrunk, EWDrunk), (100, 1000), 10)
+    simAll1((UsualDrunk, ColdDrunk, EWDrunk),
+            (10, 100, 1000, 10000, 100000), 100)
