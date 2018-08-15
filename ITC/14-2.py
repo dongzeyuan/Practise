37f7534cd6feedbad14a2fdf130b0171008db1a4
dongzeyuan <dongfujing88@gmail.com>
2018-08-15 15:15:38 +0800

    style

--- ITC/14-2.py
+++ ITC/14-2.py
@@ -78,6 +78,19 @@ class EWDrunk(Drunk):
         return random.choice(stepChoices)
 
 
+class styleIterator(object):
+    def __init__(self,styles):
+        self.index = 0
+        self.styles = styles
+    
+    def nextStyle(self):
+        result = self.styles[self.index]
+        if self.index == len(self.styles) - 1:
+            self.index = 0
+        else:
+            self.index += 1
+        return result
+
 def simAll(drunkKinds, walkLengths, numTrials):
     for dClass in drunkKinds:
         drunkTest(walkLengths, numTrials, dClass)
