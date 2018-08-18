2f937997ccb32846a4ea428dd1e8318cfe9419ab
dongzeyuan <dongfujing88@gmail.com>
2018-08-18 16:40:28 +0800

    plotdata

--- ITC/18.py
+++ ITC/18.py
@@ -1,12 +1,31 @@
+import pylab
+
+
 def getData(fileName):
     '''读取一个文件汇总的数据，返回包含距离和质量的列表'''
-    dataFile = open(fileName,'r')
+    dataFile = open(fileName, 'r')
     distances = []
     masses = []
-    dataFile.readline()  #ignore header
+    dataFile.readline()  # ignore header
     for line in dataFile:
         d, m = line.split('')
         distances.append(float(d))
         masses.append(float(m))
     dataFile.close()
     return (masses, distances)
+
+
+def plotData(inputFile):
+    masses, distances = getData(inputFile)
+    distances = pylab.array(distances)
+    masses = pylab.array(masses)
+    forces = masses*9.81
+    pylab.plot(forces, distances, 'bo',
+               label = 'Measured displacements')
+    pylab.title('Measured Displacement of Spring')
+    pylab.xlabel('|Force| (Newtons)')
+    pylab.ylabel('Distances (meters)')
+    pylab.show()
+
+if __name__ == '__main__':
+    plotData('springData.txt')
