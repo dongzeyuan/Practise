ecef4466d30c5c9e88e766b4f3df6db24959b9d3
dongzeyuan <dongfujing88@gmail.com>
2018-08-19 16:50:02 +0800

    FITDATA

--- ITC/18.py
+++ ITC/18.py
@@ -21,11 +21,31 @@ def plotData(inputFile):
     masses = pylab.array(masses)
     forces = masses*9.81
     pylab.plot(forces, distances, 'bo',
-               label = 'Measured displacements')
+               label='Measured displacements')
     pylab.title('Measured Displacement of Spring')
     pylab.xlabel('|Force| (Newtons)')
     pylab.ylabel('Distances (meters)')
     pylab.show()
 
+
+def fitData(inputFile):
+    masses, distances = getData(inputFile)
+    distances = pylab.array(distances)
+    forces = pylab.array(masses)*9.81
+    pylab.plot(forces, distances, 'ko',
+               label='Measured displacements')
+    pylab.xlabel('|Force| (Newtons)')
+    pylab.ylabel('Distances (meters)')
+    # find linear fit
+    a, b = pylab.polyfit(forces, distances, 1)
+    predictedDistances = a*pylab.array(forces)+b
+    k = 1.0/a  # see explanation in text
+    pylab.plot(forces, predictedDistances,
+               label='Displacements predicted by \n linear fit, k = '
+               + str(round(k, 5)))
+    pylab.legend(loc='best')
+    pylab.show()
+
+
 if __name__ == '__main__':
-    plotData('E:\Python\Practise\ITC\springData.txt')
+    fitData('E:\Python\Practise\ITC\springData.txt')
